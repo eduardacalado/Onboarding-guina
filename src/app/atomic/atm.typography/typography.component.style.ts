9e7595f13c8b109db9f9d74b18757b4a23d54dcc
Eduarda Calado <dudacal1600@hotmail.com>
2025-02-26 13:17:01 -0300

    refactor: styles

--- src/app/atomic/atm.typography/typography.component.style.ts
+++ src/app/atomic/atm.typography/typography.component.style.ts
@@ -1,7 +1,7 @@
 import { tv } from "tailwind-variants";
 
 export const typographyVariants = tv({
-  base: "font-medium font-semibold",
+  base: "text-medium font-semibold",
   variants: {
     variant: {
       display: "font-bold text-xx-large line-height-sm",
@@ -9,14 +9,13 @@ export const typographyVariants = tv({
       heading2: "font-semibold text-large line-height-sm",
       heading3: "font-semibold text-medium line-height-sm",
       heading4: "font-medium text-small line-height-md",
-      body1: "font-medium text-x-small text-gray-x-dark line-height-md",
+      body1: "text-x-small text-gray-x-dark line-height-md",
       body2: "font-medium text-xx-small text-gray-dark line-height-md",
-      link: "font-semibold text-x-small text-cta-dark line-height-md",
+      link: "text-x-small text-cta-dark line-height-md",
       linkSmall: "font-semibold text-xx-small text-cta-dark line-height-md",
-      inputLabel: "font-medium text-small text-gray-dark line-height-md",
-      inputValue: "font-medium text-small text-gray-medium line-height-md",
-      inputCaption:
-        "font-medium text-small text-feedback-warning-dark line-height-md",
+      inputLabel: "text-small text-gray-dark line-height-md",
+      inputValue: "text-small text-gray-medium line-height-md",
+      inputCaption: "text-small text-feedback-warning-dark line-height-md",
     },
   },
   defaultVariants: {
