9da47b3b63ede6696fdde3aa1eb094858c5a8d71
Maria Eduarda <161755667+eduardacalado@users.noreply.github.com>
2025-03-17 15:25:14 -0300

    Feature/graphql codegen (#13)
    * feature: add graphql codegen
    
    * feature: add loading spinner
    
    * fix: unnused code

--- src/app/atomic/atm.button/button.component.tsx
+++ src/app/atomic/atm.button/button.component.tsx
@@ -1,21 +1,24 @@
 import { ButtonHTMLAttributes } from "react";
 import { buttonVariants } from "./button.component.style";
 import { VariantProps } from "tailwind-variants";
+import { LoadingSpinner } from "@/app/atomic/index";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof buttonVariants> & {
     disabled?: boolean;
+    isLoading: boolean;
   };
 
 function Button({
   variant = "primary",
   disabled,
+  isLoading,
   children,
   ...props
 }: ButtonProps) {
   return (
     <button className={buttonVariants({ variant, disabled })} {...props}>
-      {children}
+      {isLoading ? <LoadingSpinner /> : children}
     </button>
   );
 }
---
+++ src/app/atomic/atm.loading-spinner/loading-spinner.component.tsx
@@ -0,0 +1,7 @@
+function LoadingSpinner() {
+  return (
+    <div className="w-md h-md rounded-full animate-spin border border-gray-light border-lg border-b-transparent" />
+  );
+}
+
+export default LoadingSpinner;
