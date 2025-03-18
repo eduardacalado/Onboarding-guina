f19b4038e56d284afae52d241b0bbd8e3f1a6984
Eduarda Calado <dudacal1600@hotmail.com>
2025-03-18 11:42:39 -0300

    feature: add home page

--- src/app/modules/auth/login.page/login.page.tsx
+++ src/app/modules/auth/login.page/login.page.tsx
@@ -12,6 +12,8 @@ import GuinaTeamImage from "../../../assets/svg/guina-team/Image.png";
 import { ArrowLeftIcon, Vector } from "@/app/assets/svg";
 import { loginStrings } from "./login.strings";
 import { useLogin } from "./login.use-case";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const formSchema = z.object({
   email: z
@@ -24,14 +26,17 @@ const formSchema = z.object({
 });
 
 export function LoginPage() {
+  const navigate = useNavigate();
+
   const { login, loading } = useLogin({
     onCompleted(data) {
-      console.log("Login realizado com sucesso", data);
+      toast.success("Login realizado com sucesso!");
+      navigate("/home");
     },
     onError(error) {
       const errorMessage =
         error.message || "Erro ao fazer login. Tente novamente";
-      console.log(errorMessage);
+      toast.error(errorMessage);
     },
   });
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,23 @@ import { App } from "./app.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "@/app/data/graphql/client.ts";
+import { ToastContainer, Bounce } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <ToastContainer
+      position="top-left"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition={Bounce}
+    />
     <ApolloProvider client={client}>
       <BrowserRouter>
         <App />
