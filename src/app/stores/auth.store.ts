f700ff950c737c67a9dbe16d4694c8f65c49ef2c
Eduarda Calado <dudacal1600@hotmail.com>
2025-03-19 13:37:51 -0300

    feature: add auth store

---
+++ src/app/stores/auth.store.ts
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+type AuthStoreProps = {
+  token: string | null;
+  setToken: (token: string) => void;
+  clearToken: () => void;
+};
+
+const defaultState: Omit<AuthStoreProps, "setToken" | "clearToken"> = {
+  token: null,
+};
+
+export const useAuthStore = create<AuthStoreProps>()(
+  persist(
+    (set) => ({
+      ...defaultState,
+      setToken: set,
+      clearToken: () => set(defaultState),
+    }),
+    {
+      name: "auth-store",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
