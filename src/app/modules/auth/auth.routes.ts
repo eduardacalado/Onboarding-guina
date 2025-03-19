622024c82d333d113f3becb042950c05c1c74715
Eduarda Calado <dudacal1600@hotmail.com>
2025-03-19 18:16:06 -0300

    feature: add register page to router

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -1,3 +1,4 @@
 export const authRoutes = {
   login: "/login",
+  register: "/register",
 };
---
+++ src/app/modules/auth/register.page/register.page.tsx
@@ -0,0 +1,7 @@
+export function RegisterPage() {
+  return (
+    <div>
+      <p>Register</p>
+    </div>
+  );
+}
