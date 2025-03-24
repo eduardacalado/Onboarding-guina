cf807834dbd261cda4872f1176e861db4dcc496c
Eduarda Calado <dudacal1600@hotmail.com>
2025-03-24 18:24:13 -0300

    feat: create logout middleware

--- src/app/data/graphql/client.ts
+++ src/app/data/graphql/client.ts
@@ -1,6 +1,38 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { useAuthStore, useUserStore } from "@/app/stores";
+import { ApolloClient, HttpLink, InMemoryCache, from } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { onError } from "@apollo/client/link/error";
+
+const httpLink = new HttpLink({
+  uri: import.meta.env.VITE_SERVER_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = useAuthStore.getState().token;
+
+  return {
+    headers: {
+      ...headers,
+      Authorization: token,
+    },
+  };
+});
+
+const logoutMiddleware = onError(({ graphQLErrors }) => {
+  graphQLErrors?.forEach((element) => {
+    if ("code" in element && element.code === 401) {
+      localStorage.removeItem("user-storage");
+      localStorage.removeItem("auth-storage");
+
+      useAuthStore.getState().clearToken();
+      useUserStore.getState().clearUser();
+
+      window.location.replace("/login");
+    }
+  });
+});
 
 export const client = new ApolloClient({
-  uri: import.meta.env.VITE_SERVER_URL,
+  link: from([authLink, httpLink, logoutMiddleware]),
   cache: new InMemoryCache(),
 });
