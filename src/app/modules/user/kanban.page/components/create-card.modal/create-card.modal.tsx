bc4cae544a5f3ff096e0261f441d18c76bd5fe52
Eduarda Calado <dudacal1600@hotmail.com>
2025-04-07 16:13:46 -0300

    refactor: create board modal

--- src/app/modules/user/kanban.page/components/create-card.modal/create-card.modal.tsx
+++ src/app/modules/user/kanban.page/components/create-card.modal/create-card.modal.tsx
@@ -2,12 +2,18 @@ import { Button, InputField, Text } from "@/app/atomic";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FormProvider, useForm } from "react-hook-form";
 import { z } from "zod";
+import { createCardStrings } from "./create-card.strings";
+import { createCardDivVariants } from "./create-card.style";
+import { toast } from "sonner";
 
 const formSchema = z.object({
   name: z.string().nonempty({ message: "Insira o nome da tarefa" }),
 });
 
 export function CreateCardModal() {
+  const { inputFieldContainer, modalContainer, titleContainer } =
+    createCardDivVariants();
+
   const methods = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -16,22 +22,22 @@ export function CreateCardModal() {
   });
 
   const handleFormSubmit = () => {
-    console.log("Card criado!");
+    toast.success(createCardStrings.succesMessage);
   };
 
   return (
-    <div className="flex flex-col w-full gap-lg">
-      <div className="flex justify-center">
-        <Text> Criar nova tarefa</Text>
+    <div className={modalContainer()}>
+      <div className={titleContainer()}>
+        <Text>{createCardStrings.createCardTitle}</Text>
       </div>
       <FormProvider {...methods}>
         <form onSubmit={methods.handleSubmit(handleFormSubmit)}>
-          <div className="flex flex-col gap-lg">
+          <div className={inputFieldContainer()}>
             <InputField
               name="name"
-              label="Nome da tarefa"
+              label={createCardStrings.input.label}
               type="text"
-              placeholder="Digite o nome da tarefa"
+              placeholder={createCardStrings.input.placeholder}
               className="flex w-full flex-col gap-sm"
             />
             <Button type="submit">Criard tarefa</Button>
---
+++ src/app/modules/user/kanban.page/components/create-card.modal/create-card.strings.ts
@@ -0,0 +1,10 @@
+export const createCardStrings = {
+  succesMessage: "Tarefa criada com sucesso!",
+
+  createCardTitle: "Criar nova tarefa",
+
+  input: {
+    label: "Nome da tarefa",
+    placeholder: "Digite o nome da tarefa",
+  },
+};
